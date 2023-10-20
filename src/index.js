7602a02b2028cce92e404905fbf124ffc303b694
Juan Esteban Garcés Medina <103276104+jegmdev@users.noreply.github.com>
2023-10-20 17:08:36 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react'
+
+const domain = process.env.react_app_auth0_domain
+const client_id = process.env.react_app_auth0_client_id
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider 
+    domain={domain} 
+    clientId={client_id} 
+    authorizationParamsredirect_uri={window.location.origin}>
+      <App/>
+    </Auth0Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
