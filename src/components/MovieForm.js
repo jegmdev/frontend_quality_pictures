148319ca7909158e83fb637a416515d722c85a06
Juan Esteban Garcés Medina <103276104+jegmdev@users.noreply.github.com>
2023-12-13 15:20:39 -0500

    Cambios realizados

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -9,7 +9,7 @@ class MovieForm extends Component {
       title: "",
       genre: "",
       synopsis: "",
-      promotionalImage: "",
+      promotionalImage: null, // Cambiado a un objeto File
       format: "2D",
       duration: "",
       ticketValue: "",
@@ -17,77 +17,127 @@ class MovieForm extends Component {
   }
 
   handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+    const { name, value, type } = event.target;
+
+    // Si el campo es un archivo, manejarlo de manera diferente
+    if (type === "file") {
+      this.setState({ [name]: event.target.files[0] });
+    } else {
+      this.setState({ [name]: value });
+    }
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    // Aquí puedes realizar acciones adicionales con los datos del formulario, como enviarlos a un servidor, etc.
-    console.log("Datos del formulario:", this.state);
+
+    // Validación de título
+    if (!this.state.title) {
+      console.error("El título es obligatorio");
+      return;
+    }
+
+    const { title, genre, synopsis, promotionalImage, format, duration, ticketValue } = this.state;
+
+    const formData = new FormData();
+    formData.append("titulo", title);
+    formData.append("genero", genre);
+    formData.append("sinopsis", synopsis);
+    formData.append("promotionalImage", promotionalImage);
+    formData.append("formato", format);
+    formData.append("duracion", duration);
+    formData.append("valor_boleta", ticketValue);
+
+    try {
+      const response = await fetch("http://localhost:3001/api/estrenos", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log("Película agregada correctamente");
+        // Puedes redirigir a otra página o realizar otras acciones después de agregar la película
+      } else {
+        console.error("Error al agregar la película");
+      }
+    } catch (error) {
+      console.error("Error de red:", error);
+    }
   };
 
   render() {
     return (
       <div className="MovieForm">
-        <h2>Agregar película</h2>
+        <h2>Agregar Película</h2>
         <form onSubmit={this.handleSubmit}>
-          <label>Título:</label>
+          <label>
+            Título:
+          </label>
           <input
-            type="text"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
           <br />
-          <label>Género:</label>
+          <label>
+            Género:
+          </label>
           <input
-            type="text"
-            name="genre"
-            value={this.state.genre}
-            onChange={this.handleChange}
-          />
+              type="text"
+              name="genre"
+              value={this.state.genre}
+              onChange={this.handleChange}
+            />
           <br />
-          <label>Sinópsis:</label>
+          <label>
+            Sinópsis:
+          </label>
           <textarea
-            name="synopsis"
-            value={this.state.synopsis}
-            onChange={this.handleChange}
-          />
+              name="synopsis"
+              value={this.state.synopsis}
+              onChange={this.handleChange}
+            />
           <br />
-          <label>Imagen promocional:</label>
+          <label>
+            Imagen promocional:
+          </label>
           <input
-            type="file"
-            name="promotionalImage"
-            value={this.state.promotionalImage}
-            onChange={this.handleChange}
-          />
+              type="file"
+              name="promotionalImage"
+              onChange={this.handleChange}
+            />
           <br />
-          <label>Formato:</label>
+          <label>
+            Formato:
+          </label>
           <select
-            name="format"
-            value={this.state.format}
-            onChange={this.handleChange}
-          >
-            <option value="2D">2D</option>
-            <option value="3D">3D</option>
-          </select>
+              name="format"
+              value={this.state.format}
+              onChange={this.handleChange}
+            >
+              <option value="2D">2D</option>
+              <option value="3D">3D</option>
+            </select>
           <br />
-          <label>Duración:</label>
+          <label>
+            Duración:
+          </label>
           <input
-            type="text"
-            name="duration"
-            value={this.state.duration}
-            onChange={this.handleChange}
-          />
+              type="text"
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handleChange}
+            />
           <br />
-          <label>Valor de la boleta:</label>
+          <label>
+            Valor de la boleta:
+          </label>
           <input
-            type="text"
-            name="ticketValue"
-            value={this.state.ticketValue}
-            onChange={this.handleChange}
-          />
+              type="text"
+              name="ticketValue"
+              value={this.state.ticketValue}
+              onChange={this.handleChange}
+            />
           <br />
           <button type="submit">Crear película</button>
         </form>
---
+++ src/layout/AdminLayout.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/Menu.css";
+
+interface AdminLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function AdminLayout({ children }: AdminLayoutProps) {
+  const [menuItemsVisible, setMenuItemsVisible] = useState(true);
+
+  const toggleMenuItems = () => {
+    setMenuItemsVisible((prevVisibility) => !prevVisibility);
+  };
+
+  return (
+    <>
+      <header>
+        <div className="div-menu">
+          <nav className="menu">
+            <div className="logo-icon-container">
+              <div className="menu-logo">
+                <img
+                  src="https://templatekit.jegtheme.com/cinomy/wp-content/uploads/sites/151/2021/08/cinomy-logo.png"
+                  alt="Logo de la App"
+                />
+              </div>
+              <div className="menu-icon" onClick={toggleMenuItems}>
+                <img
+                  src="https://i.postimg.cc/j2t41Pfd/icon-menu.png"
+                  alt="Icono del Menú"
+                />
+              </div>
+            </div>
+            <ul className={`menu-items ${menuItemsVisible ? "" : "hidden"}`}>
+              <li>
+                <Link to="/admin/peliculas">Administrar Películas</Link>
+              </li>
+              <li>
+                <Link to="/admin/peliculas">Lista Reservas</Link>
+              </li>
+              <li>
+                <Link to="/admin/peliculas">Cerrar sesión</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>{children}</main>
+    </>
+  );
+}
